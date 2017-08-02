81e99534091112dc7bccda626c3267ad0c90cc50
Jason Aden <jasonaden@google.com>
2017-08-02 15:17:02 -0700

    refactor(build): publish ESM5 instead of CommonJS

--- .make-packages.js
+++ .make-packages.js
@@ -12,25 +12,25 @@ var license = 'Apache License 2.0 ' + licenseUrl;
 
 delete pkg.scripts;
 
-var cjsPkg = Object.assign({}, pkg, {
+var esm5Pkg = Object.assign({}, pkg, {
   name: 'rxjs',
-  main: 'Rx.js',
+  module: 'Rx.js',
   typings: 'Rx.d.ts'
 });
 
-fs.writeFileSync('dist/cjs/package.json', JSON.stringify(cjsPkg, null, 2));
-fs.writeFileSync('dist/cjs/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
-fs.writeFileSync('dist/cjs/README.md', fs.readFileSync('./README.md').toString());
+fs.writeFileSync('dist/esm5/package.json', JSON.stringify(esm5Pkg, null, 2));
+fs.writeFileSync('dist/esm5/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
+fs.writeFileSync('dist/esm5/README.md', fs.readFileSync('./README.md').toString());
 
-// Bundles for CJS only
-mkdirp.sync('dist/cjs/bundles');
+// Bundles for esm5 only
+mkdirp.sync('dist/esm5/bundles');
 // UMD bundles
-fs.writeFileSync('dist/cjs/bundles/Rx.js', fs.readFileSync('dist/global/Rx.js').toString());
-fs.writeFileSync('dist/cjs/bundles/Rx.min.js', fs.readFileSync('dist/global/Rx.min.js').toString());
-fs.writeFileSync('dist/cjs/bundles/Rx.min.js.map', fs.readFileSync('dist/global/Rx.min.js.map').toString());
+fs.writeFileSync('dist/esm5/bundles/Rx.js', fs.readFileSync('dist/global/Rx.js').toString());
+fs.writeFileSync('dist/esm5/bundles/Rx.min.js', fs.readFileSync('dist/global/Rx.min.js').toString());
+fs.writeFileSync('dist/esm5/bundles/Rx.min.js.map', fs.readFileSync('dist/global/Rx.min.js.map').toString());
 
 // Add licenses to tops of bundles
-addLicenseToFile('LICENSE.txt', 'dist/cjs/bundles/Rx.js');
-addLicenseTextToFile(license, 'dist/cjs/bundles/Rx.min.js');
+addLicenseToFile('LICENSE.txt', 'dist/esm5/bundles/Rx.js');
+addLicenseTextToFile(license, 'dist/esm5/bundles/Rx.min.js');
 addLicenseToFile('LICENSE.txt', 'dist/global/Rx.js');
 addLicenseTextToFile(license, 'dist/global/Rx.min.js');
